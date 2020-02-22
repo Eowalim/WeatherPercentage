59cfa9a4cc379a34bf244b467179f09d4772d5e3
Corentin <48736362+Eowalim@users.noreply.github.com>
2020-02-22 15:09:11 +0100

    Update ChangePercentWeather.java

--- src/main/java/fr/eowalim/weatherpercentage/commands/ChangePercentWeather.java
+++ src/main/java/fr/eowalim/weatherpercentage/commands/ChangePercentWeather.java
@@ -19,57 +19,56 @@ public class ChangePercentWeather implements CommandExecutor {
             final String prefix = cfg.getString("prefix").replace("&", "§");
 
             if (label.equalsIgnoreCase("wp")) {
-              if(player.hasPermission("WeatherPercentage.wp")) {
-                  if(args.length == 0){
-                      commandList(player,prefix);
-                  } else {
-                      switch (args[0]) {
-                          case "help":
-                              commandList(player,prefix);
-                              break;
+                if (player.hasPermission("WeatherPercentage.wp")) {
+                    if (args.length == 0) {
+                        commandList(player, prefix);
+                    } else {
+                        switch (args[0]) {
+                            case "help":
+                                commandList(player, prefix);
+                                break;
 
-                          case "percent":
-                              if (args.length == 1 ) {
-                                  player.sendMessage(prefix + "The command is: §b/wp percent <valeur>");
-                              } else {
-                                  final int percent = Integer.parseInt(args[1]);
-                                  if(percent < 0 || percent > 100) {
-                                      sender.sendMessage(prefix + "§cThe value must be between 0 and 100.");
-                                  }
-                                  else if (percent == cfg.getInt("percent")) {
-                                      sender.sendMessage(prefix + "§fThe percentage is already of §c" + percent + "%§f.");
-                                  }
-                                  else {
-                                      cfg.set("percent", percent);
-                                      cfg.set("last", player.getName());
-                                      Main.getInstance().saveConfig();
-                                      Bukkit.broadcastMessage(prefix + "§aThe percentage has been changed to:§b " + args[1] + "% §7(§c" + sender.getName() +"§7)");
-                                  }
-                              }
-                              break;
+                            case "percent":
+                                if (args.length == 1) {
+                                    player.sendMessage(prefix + "The command is: §b/wp percent <valeur>");
+                                } else {
+                                    final int percent = Integer.parseInt(args[1]);
+                                    if (percent < 0 || percent > 100) {
+                                        sender.sendMessage(prefix + "§cThe value must be between 0 and 100.");
+                                    } else if (percent == cfg.getInt("percent")) {
+                                        sender.sendMessage(prefix + "§fThe percentage is already of §c" + percent + "%§f.");
+                                    } else {
+                                        cfg.set("percent", percent);
+                                        cfg.set("last", player.getName());
+                                        Main.getInstance().saveConfig();
+                                        Bukkit.broadcastMessage(prefix + "§aThe percentage has been changed to:§b " + args[1] + "% §7(§c" + sender.getName() + "§7)");
+                                    }
+                                }
+                                break;
 
-                          case "actual":
-                              String last = cfg.getString("last");
-                              player.sendMessage(prefix + "§aThe current percentage is: §b"
-                                      + Main.getInstance().getConfig().getInt("percent")
-                                      + "% §7(last edit:§c " + last + "§7)");
-                              break;
-                          default: commandList(player,prefix);
-                      }
-                  }
-              } else {
-                  player.sendMessage(prefix + "§cYou do not have permission to make this command");
-              }
+                            case "actual":
+                                String last = cfg.getString("last");
+                                player.sendMessage(prefix + "§aThe current percentage is: §b" +
+                                    Main.getInstance().getConfig().getInt("percent") +
+                                    "% §7(last edit:§c " + last + "§7)");
+                                break;
+                            default:
+                                commandList(player, prefix);
+                        }
+                    }
+                } else {
+                    player.sendMessage(prefix + "§cYou do not have permission to make this command");
+                }
             }
         }
         return false;
     }
 
-    private void commandList(Player player, String prefix){
-        player.sendMessage(prefix +"§eInvalid command. Try it: §b"
-                + "\n/wp help"
-                + "\n/wp actual"
-                + "\n/wp percent <value>"
+    private void commandList(Player player, String prefix) {
+        player.sendMessage(prefix + "§eInvalid command. Try it: §b" +
+            "\n/wp help" +
+            "\n/wp actual" +
+            "\n/wp percent <value>"
         );
     }
 }
